c6cc9fcd1fb35777a1a9de7f8b28876e76da2b31
Sirishakanakala <110217912+Sirishakanakala@users.noreply.github.com>
2023-09-22 14:42:43 +0530

    npm

--- src/components/News.js
+++ src/components/News.js
@@ -45,9 +45,9 @@ const News =(props)=> {
     // setPage(page+1)
     props.setProgress(100);
   }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+ 
   useEffect(() => {
-    // updateNews();
+    updateNews();
   }, [updateNews])
   
 
